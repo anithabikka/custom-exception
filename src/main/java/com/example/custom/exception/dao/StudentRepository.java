e4e27c7eb47bcea89806d770c044c7981f5e7133
anithabikka <145009166+anithabikka@users.noreply.github.com>
2024-01-30 10:31:39 +0530

    Initial commit

---
+++ src/main/java/com/example/custom/exception/dao/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.example.custom.exception.dao;
+
+import com.example.custom.exception.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Long> {
+}
